6fff8de45d497d5306b38cb170250f741fabaac6
HatemKhattab <hattemkhattab@gmail.com>
2016-03-19 08:32:40 +0000

    fix getting the orgnr from allabolag.se

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def getCompanyOrgnr(company_name)
+    require 'rubygems'
+    require 'nokogiri'
+    require 'open-uri'
+    page = Nokogiri::HTML(open('http://www.allabolag.se/?what='+company_name+'&where=&s.x=0&s.y=0'))
+
+    # To get the number of search results
+    result = page.css('div#printContent.hitlistBox div.hitlistTop table tr td.hitlistResultNr')
+    message = result.text
+    message.each_char{ |c| message.delete!(c) if c.ord<48 or c.ord>57 }
+    numberOfResult = message.to_i
+
+    if numberOfResult > 0
+      tables = page.css('div#printContent.hitlistBox div.hitlistRow table')
+      table=tables[0].css('tr td.text11grey6')
+      cell = table[0]
+      items = cell.children
+      item = items[2]
+      #return orgnr if it found it
+      return item.text
+    else
+      #if the search failed
+      return nil
+    end
+  end
 end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,7 @@
 #company_search
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show]
+
   def index
   end
 
@@ -10,24 +11,27 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.new(company_params)
-    @company.orgnr = "333-4444"
-    if @company.save
-      redirect_to @company
-    else
+    @company.orgnr = getCompanyOrgnr(@company.name)
+    if @company.orgnr.nil?
+      flash[:notice] = "No result!, please try again"
       render :new
+    else
+      #if the search find compnay orgnr
+      @company.save
+      redirect_to @company
     end
-  end
-
   end
 
   def show
   end
 
-  private
 
+  private
     def set_company
       @company = Company.find(params[:id])
     end
+
     def company_params
       params.require(:company).permit(:name)
     end
+end
