ad120d0db69c54e0b6bf7433f5cccee50a1da148
HatemKhattab <hattemkhattab@gmail.com>
2016-03-22 21:38:55 +0000

    add tests to model compnay

--- spec/models/compnay_spec.rb
+++ spec/models/compnay_spec.rb
@@ -1,6 +1,32 @@
-require "rails_helper"
-
 RSpec.describe Company, :type => :model do
-  it "some thing 1"
-  it "some thing 2"
+  fixtures :companies
+
+  it "accept compnay with name and orgnr" do
+  expect(companies(:ikea)).to be_valid
+  end
+
+  it "not accept compnay without name" do
+    expect(companies(:scania)).to be_invalid
+  end
+
+  it "not accept compnay without orgnr" do
+    expect(companies(:volvo)).not_to be_valid
+  end
+
+  it "not accept compnay without name and orgnr" do
+    expect(companies(:saab)).not_to be_valid
+  end
+
+  it "is invalid without name" do
+    company = Company.new(orgnr: '893436')
+    company.valid?
+    expect(company.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without orgnr" do
+    company = Company.new(name: "volvo")
+    company.valid?
+    expect(company.errors[:orgnr]).to include("can't be blank")
+  end
+
 end
