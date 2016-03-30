cd04d1c5a12fed98847b2ab60ae61d1c5f0292b0
HatemKhattab <hattemkhattab@gmail.com>
2016-03-30 15:31:19 +0000

    Add more controller tests

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -23,17 +23,35 @@ RSpec.describe CompaniesController, :type => :controller do
     end
   end
 
- describe 'post #create' do
-    describe 'unvalid' do
+  describe 'post #create' do
+    describe 'with invalid params' do
+      compnay_counter_before = Company.count
+
       before do
-        post :create, {:company => {name:"spotify"}}
+          post :create, {:company => {name:"invalidcompany"}}
+        end
+
+      it "has redirection http status" do
+        expect(response).to have_http_status(:found)
       end
-      it "has fail status"
-      it "has flash error message"
-      it "redirect to new compnay"
+
+      it "has flash error message" do
+        expect(flash[:notice]).to be_present
+        expect(flash[:notice]).to eq("searching invalidcompany  fails! , please try a nother name")
+      end
+
+      it "redirect to new compnay " do
+        expect(response).to redirect_to :new_company
+      end
+
+      it "should not increment Compnay.count" do
+        expect(Company.count - compnay_counter_before).to eq(0)
+      end
     end
-    describe 'valid' do
+
+    describe 'with valid params' do
     end
+
   end
 
  describe 'get #show' do
